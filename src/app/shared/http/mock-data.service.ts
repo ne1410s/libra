9bc449eea80d3e886f4b8c99c0eb37c8e9553822
ne1410s <pgj646@gmail.com>
2019-01-03 19:11:05 +0000

    data update

--- src/app/shared/http/mock-data.service.ts
+++ src/app/shared/http/mock-data.service.ts
@@ -19,10 +19,10 @@ export class MockDataService implements InMemoryDbService {
   createDb() {
 
     const exercises: ExerciseItem[] = [
-      new ExerciseItem('Walking', 150),
-      new ExerciseItem('Jogging', 600),
-      new ExerciseItem('Running', 800),
-      new ExerciseItem('Sprinting', 1000)
+      new ExerciseItem('Walking', 250),
+      new ExerciseItem('Jogging', 800),
+      new ExerciseItem('Running', 1150),
+      new ExerciseItem('Sprinting', 1400)
     ];
 
     const foods: FoodItem[] = [
@@ -31,7 +31,9 @@ export class MockDataService implements InMemoryDbService {
     ];
 
     const massRecords: MassRecord[] = [
-      new MassRecord(86.8, new Date(2019, 1, 1, 9, 0)),
+      new MassRecord(86.8, new Date(2019, 0, 1, 9, 0)),
+      new MassRecord(87.3, new Date(2019, 0, 2, 8, 0)),
+      new MassRecord(85.8, new Date(2019, 0, 3, 8, 0)),
       //
     ];
 
@@ -44,7 +46,8 @@ export class MockDataService implements InMemoryDbService {
     ];
 
     const exerciseRecords: ExerciseRecord[] = [
-      new ExerciseRecord(3, 20, new Date(2019, 1, 2, 12, 20)),
+      new ExerciseRecord(3, 20, new Date(2019, 0, 2, 12, 20)),
+      new ExerciseRecord(3, 19, new Date(2019, 0, 3, 12, 20)),
       //
     ];
 
